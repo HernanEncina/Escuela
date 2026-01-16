4e88b7a89a4f58139740bf033dc9168ded6ad8bb
HernanEncina <hernanhem06@gmail.com>
2026-01-15 20:01:50 -0600

    Version final

---
+++ Full-stack/act1/templates/script.js
@@ -0,0 +1,17 @@
+
+
+function modoClaro(){
+    alert("llamada a funcion js modoClaro")
+    const x = document.getElementById("encabezado");
+    const y = document.getElementById("formulario");
+    const z = document.getElementById("display");
+
+    x.style.backgroundColor= "#F2F0F0";
+    x.style.color= "#040304";
+    z.style.backgroundColor= "#F2F0F0";
+    z.style.color= "#040304";
+    y.style.backgroundColor= "#F2F0F0";
+    y.style.color= "#040304";
+    document.documentElement.style.backgroundColor="#040304";
+}
+
